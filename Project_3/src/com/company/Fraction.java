c02e2f181dabc972676060d62d3337233299d00d
John Clayton <jclayton6@student.umuc.edu>
2018-10-01 12:17:14 -0700

    Update Fraction.java

--- Project_3/src/com/company/Fraction.java
+++ Project_3/src/com/company/Fraction.java
@@ -4,46 +4,29 @@ package com.company;
 
 public class Fraction implements Comparable {
 
-    //to hold the list of fractions
+   
     private String fraction;
 
-    //constructor
-    Fraction(String fraction) {
-
-        //stores input to fraction variable
-        this.fraction = fraction;
+    Fraction(String fraction) {     // Creating a constructor
+     
+        this.fraction = fraction;   // Store to variable fraction
     }
 
-    //this method compares each fraction using cross multiplying to determine
-    // 	the result, thus determining which one is larger and smaller
     @Override
-    public int compareTo(Object input) {
+    public int compareTo(Object input) {   // Method compares by cross mutliplying fractions
+                                            // to determine which is larger or smaller
 
-        //stores the fractions to an array and separates them
-        String numbers[] = fraction.split("/");
-
-        //to hold the numerator
-        int numerator = Integer.valueOf(numbers[0]);
-
-        //to hold the denominator
-        int denominator = Integer.valueOf(numbers[1]);
-
-        //splits the fractions again to get the next fraction
-        numbers = input.toString().split("/");
-
-        //to hold the left side total of the first numerator and the second denominator
-        int leftSideTotal = numerator * Integer.valueOf(numbers[1]);
-
-        //to hold the right side total of the fist denominator and the second numerator
-        int rightSideTotal = denominator * Integer.valueOf(numbers[0]);
-
+        String numbers[] = fraction.split("/");     // Seperates values and stores then in array
+        int numerator = Integer.valueOf(numbers[0]);        // Holds the numerator
+        int denominator = Integer.valueOf(numbers[1]);      // Holds the denominator
+        numbers = input.toString().split("/");          // Splits the fraction and moves to next  
+        int leftSideTotal = numerator * Integer.valueOf(numbers[1]);    // Holds left side total, first numerator and second denominator
+        int rightSideTotal = denominator * Integer.valueOf(numbers[0]);  // Holds right side total, first denominator and second numerator
 
         return Integer.compare(leftSideTotal, rightSideTotal);  //returns 1 if the total on the left side is greater than the right side
                                                                 //returns -1 is the total on the left side is less than the right side
                                                                 //otherwise, returns 0
     }
-
-    //to String method
     @Override
     public String toString() {
         return fraction;
